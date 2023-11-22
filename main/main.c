7b9c0fed5cd678f98b45cd30ba481e5c3257e916
corbin <vytran210402@gmail.com>
2023-11-22 19:26:09 +0700

    add files

--- main/main.c
+++ main/main.c
@@ -1,46 +1,114 @@
+// #include <cstddef>
+#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include <time.h>
+#include <string.h>
 
+struct QueueData {
+    int requestID;
+    char message [20];
+    char reject;
+};
+struct TaskType {
+    int taskID;
+    char taskName [20];
+};
+QueueHandle_t xQueue;
+struct TaskType Led = {.taskName = "led", .taskID = 0};
+struct TaskType Pump = {.taskName = "pump", .taskID = 1};
+struct TaskType Fan = {.taskName = "fan", .taskID = 2};
 
-void delay_ms(int ms){
-    clock_t start = clock();
-    while ((clock() - start)*1000 /CLOCKS_PER_SEC < ms );
-}
-void Task1 (void *pvParameters) {
-    while(1){
-        printf("Task 1 (High Priority Task) is blocked...\n");
-        vTaskDelay(pdMS_TO_TICKS(2000));
+void xQueue_manager(void * pvParameter) {
+    // create a queue, each slot contant a pointer, because arcording to the website,
+    // this struct is "too big!"
+    xQueue = xQueueCreate(10, sizeof(struct QueuneData *));
+    if (xQueue == NULL) {
+    printf ("Failed to create queue: not enough RAM.");
     }
+    vTaskDelete(NULL);
 }
 
-void Task2(void *pvParameters) {
-    while(1){
-        printf("Task 2 (Medium Priority Task) is blocked...\n");
-        vTaskDelay(pdMS_TO_TICKS(2000));
+void recepiton_Task(void * pvParameter) {
+    time_t t;
+    srand ((unsigned) time(&t));
+    for (; ;) {
+        while (xQueue == NULL) {
+        // do mothing
+        }
+        int ranJob = (rand() % 4);
+        int ranDelay = (rand() % 5) + 1;
+        // allocate a dynamic space for the data, since our queue only save the "copy" of the pointer.
+        struct QueueData *xData = malloc(sizeof(struct QueueData));
+        if (xData != NULL) {
+            switch(ranJob) {
+                case 0:
+                    xData->requestID = 0;
+                    strcpy (xData->message, "LED");
+                    xData->reject = 0;
+                break;
+                case 1:
+                case 2:
+                    xData->requestID = 2;
+                    strcpy (xData->message, "FAN");
+                    xData->reject = 0;
+                break;
+                case 3:
+                    xData->requestID = 99;
+                    strcpy (xData->message, "noob");
+                    xData->reject = 0;
+                break;
+            }
+        }
+        if (xQueueSend(xQueue, (void *)&xData, 100) == errQUEUE_FULL) {
+            printf("Failed to Import job with ID %d", ranJob);
+        } else {
+            // if malloc is not successful, it will return NULL.
+            printf("Can’t allocate new struct");
+        }
+        vTaskDelay(pdMS_TO_TICKS(100 * ranDelay));
     }
+    vTaskDelete(NULL);
 }
-        
-void Task3(void *pvParameters) {
-    while(1){
-        printf("Task 3 (Low Priority Task) is running...\n");
-        delay_ms(100);
-        printf("Task 1 (High Priority Task) is ready...\n");
-        xTaskCreatePinnedToCore(Task2, "Task 2", 1000, NULL, 2, NULL, 0);
-        delay_ms(100);
-        printf("Task 2 (Medium Priority Task) is ready...\n");
-        xTaskCreatePinnedToCore(Task1, "Task 1", 1000, NULL, 3, NULL, 0);
-        delay_ms(200);
-        printf("Excecutes...\n");
-        taskYIELD();
-        vTaskDelay(pdMS_TO_TICKS(10));
+
+void active_Task(void* pvParameter) {
+    for (; ;) {
+        struct TaskType *task = (struct TaskType *)pvParameter;
+        struct QueueData *pRxMessage;
+        if (xQueue != NULL) {
+            if (xQueueReceive (xQueue, &pRxMessage, (TickType_t)10) == pdPASS) {
+                if (pRxMessage->requestID == task->taskID) {
+                    printf("%s\n", pRxMessage->message);
+                    // always remember to free the memory when done.
+                    free (pRxMessage);
+                } else {
+                    printf("%s: received %s, but it’s not my task\n",task->taskName,
+                    pRxMessage ->message);
+                    if (pRxMessage->reject < 2) {
+                        pRxMessage->reject++;
+                        xQueueSendToFront(xQueue, (void *)&pRxMessage, (TickType_t) 10);
+                    } else {
+                        printf("This task %s is rejected %d times, skiping the task\n",
+                        pRxMessage ->message, pRxMessage->reject);
+                        free (pRxMessage);
+                    }
+                }
+            } else {
+                // printf ("fan: queue empty\n");
+            } 
+        }
+        vTaskDelay (pdMS_TO_TICKS(10));
     }
+    vTaskDelete(NULL);
 }
 
-void vApplicationIdleHook (void) {
-}
 
-
-void app_main(void) {
-    xTaskCreatePinnedToCore(Task3, "Task 3", 1000, NULL, 1, NULL, 0);
+void app_main(void)
+{
+    xTaskCreate(&xQueue_manager , "queue_manage", 2048, NULL, 10, NULL);
+    xTaskCreate(&recepiton_Task, "rec", 2048, NULL, 10, NULL);
+    xTaskCreate(&active_Task, "led", 2048, (void *)&Led, 10, NULL);
+    xTaskCreate(&active_Task, "pump", 2048, (void *)&Pump, 10, NULL);
+    xTaskCreate(&active_Task, "fan", 2048, (void *)&Fan, 10, NULL);
 }
